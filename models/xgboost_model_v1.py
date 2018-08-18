220ecba133b394b1cd669db201005770f9881fbd
Grant Schneider <grant@upstart.com>
2018-08-18 14:11:18 -0700

    small updates

--- models/xgboost_model_v1.py
+++ models/xgboost_model_v1.py
@@ -10,6 +10,9 @@ DATA_DIRECTORY = os.path.expanduser("~/numerai_data/")
 napi = numerapi.NumerAPI(verbosity="info")
 current_round = napi.get_current_round()
 PATH_TO_TRAINING_DATA = f'{DATA_DIRECTORY}numerai_dataset_{current_round}/numerai_training_data.csv'
+PATH_TO_TOURNAMENT_DATA = f'{DATA_DIRECTORY}numerai_dataset_{current_round}/numerai_tournament_data.csv'
+
+number_of_training_eras = current_round - 1
 
 POSSIBLE_MARKETS = [
   'target_bernie', 'target_charles', 'target_elizabeth', 'target_jordan', 'target_ken'
@@ -43,21 +46,25 @@ def _construct_X_and_Y_for_training(training_df, target_name):
 
 
 full_training_df = pd.read_csv(PATH_TO_TRAINING_DATA, header = 0)
+full_tournament_df = pd.read_csv(PATH_TO_TOURNAMENT_DATA, header = 0)
 
 full_training_df['era_numeric'] = full_training_df['era'].apply(_numeric_era_converter)
+full_tournament_df['era_numeric'] = full_tournament_df['era'].apply(_numeric_era_converter)
 
+
+# TODO: This subsetting is only necessary if we change number_of_training_eras
 training_df_limited_era = _subset_training_data_by_era(
-  training_df = full_training_df, era_numeric = 100
+  training_df = full_training_df, era_numeric = number_of_training_eras
 )
 
-training_data_X_100_eras, training_data_Y_100_eras = _construct_X_and_Y_for_training(
+training_data_X_limited_eras, training_data_Y_limited_eras = _construct_X_and_Y_for_training(
   training_df = training_df_limited_era, target_name = TARGET_NAME
 )
 
 
 X_train, X_test, y_train, y_test = train_test_split(
-  training_data_X_100_eras,
-  training_data_Y_100_eras,
+  training_data_X_limited_eras,
+  training_data_Y_limited_eras,
   test_size=0.2,
   random_state=0
 )
@@ -67,16 +74,16 @@ dtest = xgboost.DMatrix(X_test, label=y_test)
 
 gpu_res = {} # Store accuracy result
 
-num_round = 3000
-params = {#'max_depth': 5,
-          #'learning_rate': 0.02,
+num_round = 1000
+params = {'max_depth': 5,
+          'learning_rate': 0.1,
           'objective': 'binary:logistic',
           #'min_child_weight': 0.1,
           #'gamma': 0.5,
           #'n_estimators': 1300,
           #'scale_pos_weight': 1,
           #'nthread': 1,
-          'tree_method': 'gpu_hist',
+          #'tree_method': 'gpu_hist',
           #'gpu_id': 0,
           #'max_bin': 16,
           #'subsample': 0.66,
