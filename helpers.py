b05a770418c65d05bd3d558c99e956b508e68faa
Grant Schneider <grant@upstart.com>
2018-08-18 21:00:46 -0700

    get rid of useless variable

--- helpers.py
+++ helpers.py
@@ -9,6 +9,6 @@ POSSIBLE_MARKET_NAMES = [
 ]
 
 def numeric_era_converter(era_string):
-  era_string_formatted = era_string.replace('era', '')
-  era_string_formatted = era_string_formatted.replace('X', '999')
-  return int(era_string_formatted)
+  era_string = era_string.replace('era', '')
+  era_string = era_string.replace('X', '999')
+  return int(era_string)
